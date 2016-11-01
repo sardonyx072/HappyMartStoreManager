5446a7478d208bf0f79afb72fe47651e57631683
sardonyx072 <mithoffman11@gmail.com>
2016-10-31 23:14:15 -0500

    second commit

---
+++ src/com/happymart/tests/ItemTypeTest.java
@@ -0,0 +1,14 @@
+package com.happymart.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ItemTypeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
